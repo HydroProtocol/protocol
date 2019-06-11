099de26bf271d4656e43eaf3756f028a350f4b55
david <davidqhr@gmail.com>
2019-06-11 18:27:31 +0800

    fix truffle js

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,10 @@
 module.exports = {
     port: 6545,
-    skipFiles: ['helper/TestMath.sol', 'helper/TestToken.sol', 'helper/StandardToken.sol'],
+    skipFiles: [
+        'helper/TestMath.sol',
+        'helper/TestToken.sol',
+        'helper/StandardToken.sol',
+        'lib/Consts.sol'
+    ],
     testrpcOptions: '--port 6545 -g 1'
 };
