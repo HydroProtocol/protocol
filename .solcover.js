0bf2d2f2ec8f999896cf8a3fd9b1b0f122b9cbac
david <davidqhr@gmail.com>
2019-06-11 16:20:31 +0800

    skip StandardToken.sol for coverage

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: 6545,
-    skipFiles: ['helper/TestMath.sol', 'helper/TestToken.sol'],
+    skipFiles: ['helper/TestMath.sol', 'helper/TestToken.sol', 'helper/StandardToken.sol'],
     testrpcOptions: '--port 6545 -g 1'
 };
