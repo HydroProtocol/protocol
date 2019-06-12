e99918837344e3416415342a915491e8a01086a5
david <davidqhr@gmail.com>
2019-06-12 13:56:05 +0800

    add evm mineAt

--- test/utils/evm.js
+++ test/utils/evm.js
@@ -1,6 +1,7 @@
 const jsonRpcCall = (method, params = []) =>
     new Promise((resolve, reject) => {
-        web3.currentProvider.send({
+        web3.currentProvider.send(
+            {
                 method,
                 params,
                 jsonrpc: '2.0',
@@ -18,26 +19,23 @@ const jsonRpcCall = (method, params = []) =>
 
 const snapshot = () => jsonRpcCall('evm_snapshot');
 const revert = snapshotID => jsonRpcCall('evm_revert', [snapshotID]);
+const minerStop = () => jsonRpcCall('miner_stop');
+const minerStart = () => jsonRpcCall('miner_start');
+const mine = timestamp => jsonRpcCall('evm_mine', timestamp ? [timestamp] : []);
+
+const mineAt = async (fn, timestamp) => {
+    await minerStop();
+
+    try {
+        const promise = fn();
+        await mine(timestamp);
+        await promise;
+    } finally {
+        await minerStart();
+    }
+};
 
 const mineEmptyBlock = async count => {
-    const mine = () =>
-        new Promise((resolve, reject) => {
-            web3.currentProvider.send({
-                    method: 'evm_mine',
-                    params: [],
-                    jsonrpc: '2.0',
-                    id: new Date().getTime()
-                },
-                (error, result) => {
-                    if (error) {
-                        reject(error);
-                    }
-
-                    resolve(result.result);
-                }
-            );
-        });
-
     const finish = [];
 
     for (let i = 0; i < count; i++) {
@@ -49,7 +47,8 @@ const mineEmptyBlock = async count => {
 
 const updateTimestamp = timestamp =>
     new Promise((resolve, reject) => {
-        web3.currentProvider.send({
+        web3.currentProvider.send(
+            {
                 method: 'evm_mine',
                 params: [timestamp],
                 jsonrpc: '2.0',
