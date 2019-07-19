5f67bc5ba8d05c6fbe911e3137695352a5b72149
david <davidqhr@gmail.com>
2019-07-19 10:39:02 +0800

    add comment for IPriceOracle

--- contracts/interfaces/IPriceOracle.sol
+++ contracts/interfaces/IPriceOracle.sol
@@ -1,7 +1,14 @@
 pragma solidity 0.5.8;
 
+/**
+ * Get USD price of asset,
+ * The return value is the USD value of the 10**18 lowest units of the asset.
+ *
+ * For example, Ether has a 18 decimals, so the price is for each Ether.
+ * But for a token has 10 decimals, the price is for 10*8 tokens.
+ */
 interface IPriceOracle {
-    /** return USD price of token, uint is 10**18 */
+    /** return USD price of token */
     function getPrice(
         address asset
     )
